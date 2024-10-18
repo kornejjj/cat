d0bc729674d86818b3efd23d4eb238e08adedad7
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-10-18 14:28:15 +0200

    HttpImageStatusCli.java

--- src/main/java/com/example/HttpImageStatusCli.java
+++ src/main/java/com/example/HttpImageStatusCli.java
@@ -1,2 +1,26 @@
-package com.example;public class HttpImageStatusCli {
+package com.example;
+import java.util.Scanner;
+
+public class HttpImageStatusCli {
+    HttpStatusImageDownloader downloader = new HttpStatusImageDownloader();
+    public void askStatus(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter HTTP status code:");
+        String enteredStatusCode = in.nextLine();
+
+        Integer verifedStatusCode=checkStatusCode(enteredStatusCode);
+        downloader.downloadStatusImage(verifedStatusCode);
+    }
+    private Integer checkStatusCode(String enteredStatusCode){
+        if(enteredStatusCode.isEmpty()){
+            throw new RuntimeException("Please enter valid number");
+        }
+        try {
+            int statusCode = Integer.parseInt(enteredStatusCode.strip());
+            return statusCode;
+        } catch (Exception e) {
+            throw new RuntimeException("Please enter valid number");
+        }
+    }
 }
