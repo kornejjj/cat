df6594d474e0d14877ef0a3c129389884eb8a82e
kornejjj <165379747+kornejjj@users.noreply.github.com>
2024-10-18 14:33:46 +0200

    HttpStatusImageDownloader.java

--- src/main/java/com/example/HttpStatusImageDownloader.java
+++ src/main/java/com/example/HttpStatusImageDownloader.java
@@ -1,2 +1,39 @@
-package com.example;public class HttpStatusImageDownloader {
+package com.example;
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+
+public class HttpStatusImageDownloader {
+    HttpStatusChecker checker=new HttpStatusChecker();
+    public void downloadStatusImage(int code){
+        if(checker.getStatusImage(code)==null){
+            return;
+        }
+        try {
+            URI uri = new URI("https", "http.cat", "/"+code+".jpg", null);
+
+            URL url = uri.toURL();
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+            connection.setRequestMethod("GET");
+
+            int responseCode = connection.getResponseCode();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            BufferedImage newImage = ImageIO.read(connection.getInputStream());
+            ImageIO.write(newImage, "jpg", new File("outputImage.jpg"));
+
+            reader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
